f67082a4beea472d7bc3677e0363bdeb1ce07454
mnyu123 <whdudwo5582@gmail.com>
2022-11-02 00:00:09 +0900

    #11-1 (수정2)

--- app/src/main/java/com/example/covid/map_java.java
+++ app/src/main/java/com/example/covid/map_java.java
@@ -1,9 +1,12 @@
 package com.example.covid;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
 
 public class map_java extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class map_java extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedIntenceStae) {
         super.onCreate(savedIntenceStae);
         setContentView(R.layout.map);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 
 }
